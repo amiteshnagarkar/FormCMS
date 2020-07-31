81b0203712eb4cab9bffcb14c8ceb404dee24fd1
amiteshnagarkar <amiteshnagarkar@gmail.com>
2020-07-31 22:44:23 +0100

    added an edit html file etc

--- formcms/views.py
+++ formcms/views.py
@@ -10,6 +10,11 @@ def employee_list(request):
     context = {'employee_list': Employee.objects.all()}
     return render(request, "formcms/employee_list.html", context)
 
+def employee_edit(request):
+    context = {'employee_list': Employee.objects.all()}
+    return render(request, "formcms/employee_edit.html", context)
+
+
 def employee_form(request, id=0):
     if request.method == "GET":
         if id==0:
